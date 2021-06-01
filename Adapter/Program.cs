80bc98387c5c437157494b6ce9c2f4df40999e5f
jsribar <julijan.sribar@gmail.com>
2021-06-01 22:37:40 +0200

    Add adapter pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DesignPatterns.Adapter
+{
+    class XmlPersonAdapter
+    {
+        public XmlPersonAdapter(string filename)
+        {
+            doc.Load(filename);
+        }
+
+        public string[] GetNames()
+        {
+            List<string> result = new List<string>();
+            foreach (var element in doc.GetElementsByTagName("name"))
+            {
+                result.Add(((XmlElement)element).InnerText);
+            }
+            return result.ToArray();
+        }
+        // https://docs.microsoft.com/en-us/dotnet/api/system.xml.xmldocument
+        XmlDocument doc = new XmlDocument();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlPersonAdapter adapter = new XmlPersonAdapter("names.xml");
+            foreach (var name in adapter.GetNames())
+                Console.WriteLine(name);
+        }
+    }
+}
