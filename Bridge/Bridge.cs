74038651843bd91a61d798a501da4ea1c383a69d
jsribar <julijan.sribar@gmail.com>
2021-06-01 23:33:08 +0200

    Add Bridge

---
+++ Bridge/Bridge.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Bridge
+{
+    class Writer
+    {
+        private IWriterImplementation implementation;
+
+        public IWriterImplementation Implementation
+        {
+            set { implementation = value; }
+        }
+
+        public void WriteName(string name)
+        {
+            implementation.WriteName(name);
+        }
+
+        public void WriteAddress(string address)
+        {
+            implementation.WriteAddress(address);
+        }
+    }
+
+    interface IWriterImplementation
+    {
+        void WriteName(string name);
+        void WriteAddress(string address);
+    }
+
+    class XmlPersonWriter : IWriterImplementation, IDisposable
+    {
+        public XmlPersonWriter(string filename)
+        {
+            streamWriter = new StreamWriter(filename);
+            streamWriter.Write("<doc>");
+        }
+
+        StreamWriter streamWriter;
+
+        public void WriteName(string name)
+        {
+            streamWriter.Write($"<name>{name}</name>");
+        }
+
+        public void Dispose()
+        {
+            streamWriter.Write("</doc>");
+            streamWriter.Close();
+        }
+
+        public void WriteAddress(string address)
+        {
+            streamWriter.Write($"<address>{address}</address>");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (XmlPersonWriter xml = new XmlPersonWriter("output.xml"))
+            {
+                Writer w = new Writer();
+                w.Implementation = xml;
+                w.WriteName("Pero");
+                w.WriteAddress("Vlaška 8");
+            }
+        }
+    }
+}
