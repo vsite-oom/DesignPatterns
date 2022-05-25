6652aab41d530b4395a4c7a80cfe9a8b11e44473
Danijel Krpes <dakrpes@vsite.hr>
2022-05-25 20:27:11 +0200

    Add rectangle to the drawing app

--- Decorator/Shape.cs
+++ Decorator/Shape.cs
@@ -54,6 +54,20 @@ namespace DesignPatterns.Decorator
             g.DrawEllipse(Pens.Black, shape.Bounds);
         }
     }
+
+    class RectangleOutline : ShapeDecorator
+    {
+        public RectangleOutline(Shape shape) : base(shape)
+        {
+        }
+
+        public override void Draw(Graphics g)
+        {
+            shape.Draw(g);
+            g.DrawRectangle(Pens.Blue, shape.Bounds);
+        }
+    }
+
     class EllipseFill : ShapeDecorator
     {
         public EllipseFill(Shape shape) : base(shape)
@@ -66,6 +80,20 @@ namespace DesignPatterns.Decorator
             g.FillEllipse(Brushes.Cyan, shape.Bounds);
         }
     }
+
+    class RectangleFill : ShapeDecorator
+    {
+        public RectangleFill(Shape shape) : base(shape)
+        {
+        }
+
+        public override void Draw(Graphics g)
+        {
+            shape.Draw(g);
+            g.FillRectangle(Brushes.GreenYellow, shape.Bounds);
+        }
+    }
+
     class ShapeCaption : ShapeDecorator
     {
         public ShapeCaption(Shape shape) : base(shape)
