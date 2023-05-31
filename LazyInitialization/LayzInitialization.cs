17598ed517fbaac18698b14ba9bf2946991a8672
Dragana Kovacevic <dragana.kovacevic@blc.edu.ba>
2023-05-31 20:29:09 +0200

    Implement Lazy Initialization

--- LazyInitialization/LayzInitialization.cs
+++ LazyInitialization/LayzInitialization.cs
@@ -16,8 +16,15 @@
 
         public Car GetCar(string model)
         {
+            
             // TODO: 016 Implement method so that objecst are created on demand.
-            return null;
+            if (availableCars.TryGetValue(model, out Car car))
+            {
+                return car;
+            }
+            car = new Car(model);
+            availableCars.Add(model, car);
+            return car;
         }
     }
 
