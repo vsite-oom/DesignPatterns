f4aa8f62dd5099e97dd334a5b306f520ad88dee3
Julijan Šribar <julijan.sribar@gmail.com>
2023-06-07 08:08:43 +0200

    Add Null-object pattern

---
+++ NullObject/ConsoleLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.NullObject
+{
+    internal class ConsoleLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
