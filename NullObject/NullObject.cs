9796198a7efa9210ba9ab8b76e14b35a9067aa1a
Julijan Šribar <julijan.sribar@gmail.com>
2023-06-07 08:08:43 +0200

    Add Null-object pattern

---
+++ NullObject/NullObject.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.NullObject
+{
+    internal class Program
+    {
+        private static void DoSomething(ConsoleLogger logger)
+        {
+            logger.Log($"I am in '{System.Reflection.MethodBase.GetCurrentMethod().Name}'");
+        }
+
+        private static void DoSomethingElse(ConsoleLogger logger)
+        {
+            logger.Log($"I am in '{System.Reflection.MethodBase.GetCurrentMethod().Name}'");
+        }
+
+        // TODO: 038 Add NullLogger class to prevent logging output
+
+        private static void Main(string[] args)
+        {
+            var logger = new ConsoleLogger();
+
+            DoSomething(logger);
+            DoSomethingElse(logger);
+            DoSomething(logger);
+        }
+    }
+}
