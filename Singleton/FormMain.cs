5d7fcf356fcc377dc6f21442e153ff51e3116a35
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-05-29 19:58:42 +0200

    Implement Singleton

--- Singleton/FormMain.cs
+++ Singleton/FormMain.cs
@@ -10,9 +10,15 @@
         private void button1_Click(object sender, EventArgs e)
         {
             // TODO: 1.4a Fetch and display value from UserSingleton class
+            UserSingleton.Instance.GetValue();
         }
 
         // TODO: 1.4b Add a button to the main form that will open a new instance of the form.
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            var newForm = new FormMain();
+            newForm.ShowDialog(this);
+        }
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
@@ -24,5 +30,7 @@
             var newForm = new FormMain();
             newForm.ShowDialog(this);
         }
+
+
     }
 }
