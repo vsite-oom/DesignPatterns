38cca6b65713e1bf71ef8d7123c3b1974727b9ec
Marko Pavić <mapavic@vsite.hr>
2024-05-29 19:17:21 +0200

    Implement builder

--- Builder/PersonDataBuilder.cs
+++ Builder/PersonDataBuilder.cs
@@ -1,36 +1,60 @@
-﻿namespace DesignPatterns.Builder
+﻿using System.Xml.Linq;
+
+namespace DesignPatterns.Builder
 {
-    interface IPersonDataBuilder
-    {
-        string BuildPrologue();
-        string BuildName(string name);
-        string BuildAddress(string street, string city);
-        string BuildEpilogue();
-    }
+	interface IPersonDataBuilder
+	{
+		string BuildPrologue();
+		string BuildName(string name);
+		string BuildAddress(string street, string city);
+		string BuildEpilogue();
+	}
 
-    class XmlPersonDataBuilder : IPersonDataBuilder
-    {
-        public string BuildAddress(string street, string city)
-        {
-            return $"<Address><StreetAddress>{street}</StreetAddress><City>{city}</City></Address>";
-        }
+	class XmlPersonDataBuilder : IPersonDataBuilder
+	{
+		public string BuildAddress(string street, string city)
+		{
+			return $"<Address><StreetAddress>{street}</StreetAddress><City>{city}</City></Address>";
+		}
 
-        public string BuildEpilogue()
-        {
-            return "</Person>";
-        }
+		public string BuildEpilogue()
+		{
+			return "</Person>";
+		}
 
-        public string BuildName(string name)
-        {
-            return $"<Name>{name}</Name>";
-        }
+		public string BuildName(string name)
+		{
+			return $"<Name>{name}</Name>";
+		}
 
-        public string BuildPrologue()
-        {
-            return "<Person>";
-        }
-    }
+		public string BuildPrologue()
+		{
+			return "<Person>";
+		}
+	}
 
-    // TODO: 1.2 Create JsonPersonDataBuilder class and uncomment corresponding code in Main method
+	// TODO: 1.2 Create JsonPersonDataBuilder class and uncomment corresponding code in Main method
 
+	class JsonPersonDataBuilder : IPersonDataBuilder
+	{
+		public string BuildAddress(string street, string city)
+		{
+			return $"\"Address\": {{\"street\": \"{street}\", \"city\":\"{city}\"}}";
+		}
+
+		public string BuildEpilogue()
+		{
+			return "}";
+		}
+
+		public string BuildName(string name)
+		{
+			return $"\"Name:\" \"{name}\"";
+		}
+
+		public string BuildPrologue()
+		{
+			return "{";
+		}
+	}
 }
