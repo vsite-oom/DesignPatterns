e90f49ead62c7593d6b8f029962b061ef1cc7f8a
Kristian Bolarić <kristian.bolaric@gmail.com>
2024-05-29 18:40:27 +0200

    Create FactoryMethod

--- FactoryMethod/FactoryMethod.cs
+++ FactoryMethod/FactoryMethod.cs
@@ -1,5 +1,22 @@
 ﻿namespace DesignPatterns.FactoryMethod
 {
+    static class ShootingTacticFactory
+    {
+        public static IShootingTactics CreateShootingTactics(CurrentShootingTactics shootingTactics)
+        {
+            switch (shootingTactics)
+            {
+                case CurrentShootingTactics.Random:
+                    return new RandomShooting();
+                case CurrentShootingTactics.Zone:
+                    return new ZoneShooting();
+                case CurrentShootingTactics.Line:
+                    return new LineShooting();
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
     static internal class Program
     {
         // TODO: 1.0 Create Factory method to be used by Gunnery class.
