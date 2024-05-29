642b6180d1b03b4ea23d8c6c2661a43d400eb734
Sonja Perković <soperkov@vsite.hr>
2024-05-29 19:03:08 +0200

    Implement Abstract Factory

--- AbstractFactory/DrawingPane.cs
+++ AbstractFactory/DrawingPane.cs
@@ -2,11 +2,17 @@
 {
     class DrawingPane : PictureBox
     {
-        private IEnumerable<Shape> shapes = new List<Shape>();
+        private List<Shape> shapes = new List<Shape>();
 
         public void Assign(IEnumerable<Shape> shapes)
         {
-            this.shapes = shapes;
+            this.shapes = shapes.ToList();
+            Invalidate();
+        }
+
+        public void Add(IEnumerable<Shape> shapes)
+        {
+            this.shapes.AddRange(shapes);
             Invalidate();
         }
 
