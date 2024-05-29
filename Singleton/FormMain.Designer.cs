40200a5ba0fed837268e1a1dddfb205c9c410349
Marko Pavić <mapavic@vsite.hr>
2024-05-29 19:58:33 +0200

    Implement singleton

--- Singleton/FormMain.Designer.cs
+++ Singleton/FormMain.Designer.cs
@@ -21,84 +21,98 @@ namespace DesignPatterns.Singleton
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            button1 = new Button();
-            textBoxData = new TextBox();
-            label1 = new Label();
-            buttonClear = new Button();
-            SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(18, 63);
-            button1.Margin = new Padding(4, 3, 4, 3);
-            button1.Name = "button1";
-            button1.Size = new Size(88, 27);
-            button1.TabIndex = 0;
-            button1.Text = "&Get data";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // textBoxData
-            // 
-            textBoxData.Location = new Point(59, 14);
-            textBoxData.Margin = new Padding(4, 3, 4, 3);
-            textBoxData.Name = "textBoxData";
-            textBoxData.Size = new Size(152, 23);
-            textBoxData.TabIndex = 1;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(14, 17);
-            label1.Margin = new Padding(4, 0, 4, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(34, 15);
-            label1.TabIndex = 2;
-            label1.Text = "&Data:";
-            // 
-            // buttonClear
-            // 
-            buttonClear.Location = new Point(125, 63);
-            buttonClear.Margin = new Padding(4, 3, 4, 3);
-            buttonClear.Name = "buttonClear";
-            buttonClear.Size = new Size(88, 27);
-            buttonClear.TabIndex = 3;
-            buttonClear.Text = "&Clear data";
-            buttonClear.UseVisualStyleBackColor = true;
-            buttonClear.Click += buttonClear_Click;
-            // 
-            // FormMain
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(236, 105);
-            Controls.Add(buttonClear);
-            Controls.Add(label1);
-            Controls.Add(textBoxData);
-            Controls.Add(button1);
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            Margin = new Padding(4, 3, 4, 3);
-            MaximizeBox = false;
-            Name = "FormMain";
-            Text = "Main Form";
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			button1 = new Button();
+			textBoxData = new TextBox();
+			label1 = new Label();
+			buttonClear = new Button();
+			button2 = new Button();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(21, 84);
+			button1.Margin = new Padding(5, 4, 5, 4);
+			button1.Name = "button1";
+			button1.Size = new Size(101, 36);
+			button1.TabIndex = 0;
+			button1.Text = "&Get data";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// textBoxData
+			// 
+			textBoxData.Location = new Point(67, 19);
+			textBoxData.Margin = new Padding(5, 4, 5, 4);
+			textBoxData.Name = "textBoxData";
+			textBoxData.Size = new Size(173, 27);
+			textBoxData.TabIndex = 1;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(16, 23);
+			label1.Margin = new Padding(5, 0, 5, 0);
+			label1.Name = "label1";
+			label1.Size = new Size(44, 20);
+			label1.TabIndex = 2;
+			label1.Text = "&Data:";
+			// 
+			// buttonClear
+			// 
+			buttonClear.Location = new Point(143, 84);
+			buttonClear.Margin = new Padding(5, 4, 5, 4);
+			buttonClear.Name = "buttonClear";
+			buttonClear.Size = new Size(101, 36);
+			buttonClear.TabIndex = 3;
+			buttonClear.Text = "&Clear data";
+			buttonClear.UseVisualStyleBackColor = true;
+			buttonClear.Click += buttonClear_Click;
+			// 
+			// button2
+			// 
+			button2.Location = new Point(21, 129);
+			button2.Margin = new Padding(5, 4, 5, 4);
+			button2.Name = "button2";
+			button2.Size = new Size(101, 36);
+			button2.TabIndex = 4;
+			button2.Text = "Next";
+			button2.UseVisualStyleBackColor = true;
+			button2.Click += button2_Click;
+			// 
+			// FormMain
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(270, 178);
+			Controls.Add(button2);
+			Controls.Add(buttonClear);
+			Controls.Add(label1);
+			Controls.Add(textBoxData);
+			Controls.Add(button1);
+			FormBorderStyle = FormBorderStyle.FixedDialog;
+			Margin = new Padding(5, 4, 5, 4);
+			MaximizeBox = false;
+			Name = "FormMain";
+			Text = "Main Form";
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private Button button1;
+		private Button button1;
         private TextBox textBoxData;
         private Label label1;
         private Button buttonClear;
-    }
+		private Button button2;
+	}
 }
 
--- Singleton/FormMain.cs
+++ Singleton/FormMain.cs
@@ -1,28 +1,35 @@
 ﻿namespace DesignPatterns.Singleton
 {
-    public partial class FormMain : Form
-    {
-        public FormMain()
-        {
-            InitializeComponent();
-        }
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // TODO: 1.4a Fetch and display value from UserSingleton class
-        }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			UserSingleton.Instance.GetValue();
+			// TODO: 1.4a Fetch and display value from UserSingleton class
+		}
 
-        // TODO: 1.4b Add a button to the main form that will open a new instance of the form.
+		// TODO: 1.4b Add a button to the main form that will open a new instance of the form.
 
-        private void buttonClear_Click(object sender, EventArgs e)
-        {
-            textBoxData.Clear();
-        }
+		private void buttonClear_Click(object sender, EventArgs e)
+		{
+			textBoxData.Clear();
+		}
 
-        private void buttonNewForm_Click(object sender, EventArgs e)
-        {
-            var newForm = new FormMain();
-            newForm.ShowDialog(this);
-        }
-    }
+		private void buttonNewForm_Click(object sender, EventArgs e)
+		{
+			var newForm = new FormMain();
+			newForm.ShowDialog(this);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			var newForm = new FormMain();
+			newForm.ShowDialog(this);
+		}
+	}
 }
