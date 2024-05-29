34d046df732d2d266a3f5e09f4e87dbc98a171a0
Đuro Rukavina <djrukavina@vsite.hr>
2024-05-29 19:02:56 +0200

    Implement AbstractFactory

--- AbstractFactory/AbstractFactory.cs
+++ AbstractFactory/AbstractFactory.cs
@@ -30,6 +30,18 @@
     }
 
     // TODO: 1.1a Add EllipseShape derived from Shape class.
+    internal class EllipseShape : Shape
+    {
+        public EllipseShape(Rectangle bounds, Pen outlinePen, Brush fillBrush) : base(bounds, outlinePen, fillBrush)
+        {
+
+        }
+        public override void Draw(Graphics g)
+        {
+            g.FillEllipse(fillBrush, bounds);
+            g.DrawEllipse(outlinePen, bounds);
+        }
+    }
 
     // TODO: 1.1b Extend ShapesFactory classes with CreateEllipses methods.
 
@@ -39,6 +51,10 @@
         public IEnumerable<RectangleShape> CreateRectangles(IEnumerable<Rectangle> bounds)
         {
             return PrepareRectangles(bounds);
+        }
+        public IEnumerable<EllipseShape> CreateEllipses(IEnumerable<Rectangle> bounds)
+        {
+            return PrepareEllipses(bounds);
         }
 
         protected virtual IEnumerable<RectangleShape> CreateRectangles(IEnumerable<Rectangle> bounds, Pen pen, Brush brush)
@@ -50,8 +66,18 @@
             }
             return rectangles;
         }
+        protected virtual IEnumerable<EllipseShape> CreateEllipses(IEnumerable<Rectangle> bounds, Pen pen, Brush brush)
+        {
+            var rectangles = new List<EllipseShape>();
+            foreach (var bound in bounds)
+            {
+                rectangles.Add(new EllipseShape(bound, pen, brush));
+            }
+            return rectangles;
+        }
 
         protected abstract IEnumerable<RectangleShape> PrepareRectangles(IEnumerable<Rectangle> bounds);
+        protected abstract IEnumerable<EllipseShape> PrepareEllipses(IEnumerable<Rectangle> bounds);
     }
 
     internal class DraftShapesFactory : AbstractShapesFactory
@@ -62,6 +88,12 @@
             pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
             return CreateRectangles(bounds, pen, Brushes.Transparent);
         }
+        protected override IEnumerable<EllipseShape> PrepareEllipses(IEnumerable<Rectangle> bounds)
+        {
+            var pen = new Pen(Color.Blue);
+            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            return CreateEllipses(bounds, pen, Brushes.Transparent);
+        }
     }
 
     internal class FilledShapesFactory : AbstractShapesFactory
@@ -69,6 +101,10 @@
         protected override IEnumerable<RectangleShape> PrepareRectangles(IEnumerable<Rectangle> bounds)
         {
             return CreateRectangles(bounds, Pens.Blue, Brushes.LightGoldenrodYellow);
+        }
+        protected override IEnumerable<EllipseShape> PrepareEllipses(IEnumerable<Rectangle> bounds)
+        {
+            return CreateEllipses(bounds, Pens.MistyRose, Brushes.OldLace);
         }
     }
 }
