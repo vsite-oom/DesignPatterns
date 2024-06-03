6dadf38d4b4a35955ddd4ae824a037265af544e9
Hrvoje Janđel <hrjandje@vsite.hr>
2024-06-03 17:00:50 +0200

    implement factory method

--- FactoryMethod/FactoryMethod.cs
+++ FactoryMethod/FactoryMethod.cs
@@ -3,7 +3,23 @@
     static internal class Program
     {
         // TODO: 1.0 Create Factory method to be used by Gunnery class.
-
+        class ShootingTacticsFactory
+        {
+            public static IShootingTactics CreateShootingTactics(CurrentShootingTactics shootingTactics)
+            {
+                switch (shootingTactics)
+                {
+                    case CurrentShootingTactics.Random:
+                        return new RandomShooting();
+                    case CurrentShootingTactics.Zone:
+                        return new ZoneShooting();
+                    case CurrentShootingTactics.Line:
+                        return new LineShooting();
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+        }
         static void Main()
         {
             var gunnery = new Gunnery();
