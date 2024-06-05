3df4aeabc33271d7a7fb983423c71290a6dc28a6
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-06-05 19:31:43 +0200

    Implement Composite

--- Composite/Composite.cs
+++ Composite/Composite.cs
@@ -2,6 +2,29 @@
 {
     // TODO: 2.2a Create Selection class that implements Composite pattern and implements IShape interface.
 
+    class Selection : Shape
+    {
+        private readonly List<Shape> shapes = new List<Shape>();
+
+        public void AddShape(Shape shape)
+        {
+            shapes.Add(shape);
+        }
+
+        public void RemoveShape(Shape shape)
+        {
+            shapes.Remove(shape);
+        }
+
+        public void Move(int dx, int dy)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Move(dx, dy);
+            }
+        }
+    }
+
     static internal class Program
     {
         static void Main(string[] args)
@@ -13,22 +36,22 @@
             // TODO: 2.2b Uncomment lines below and test the functionality.
 
             Console.WriteLine("Move first selection:");
-            //var sel = new Selection();
-            //sel.AddShape(c1);
-            //sel.AddShape(r1);
-            //sel.AddShape(c2);
+            var sel = new Selection();
+            sel.AddShape(c1);
+            sel.AddShape(r1);
+            sel.AddShape(c2);
 
-            //sel.Move(3, 4);
+            sel.Move(3, 4);
 
-            //Console.WriteLine("Move selection with removed c1:");
-            //sel.RemoveShape(c1);
-            //sel.Move(1, 0);
+            Console.WriteLine("Move selection with removed c1:");
+            sel.RemoveShape(c1);
+            sel.Move(1, 0);
 
-            //Console.WriteLine("Move another selection:");
-            //var anotherSel = new Selection();
-            //anotherSel.AddShape(sel);
-            //anotherSel.AddShape(c1);
-            //anotherSel.Move(2, -3);
+            Console.WriteLine("Move another selection:");
+            var anotherSel = new Selection();
+            anotherSel.AddShape(sel);
+            anotherSel.AddShape(c1);
+            anotherSel.Move(2, -3);
         }
     }
 }
