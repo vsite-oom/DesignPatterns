b3767b9b0ad279d57fcb79abbae1f1db1e9fd48d
Vlado Tonković <vltonkov@vsite.hr>
2024-06-05 19:31:47 +0200

    Implement Composite

--- Composite/Composite.cs
+++ Composite/Composite.cs
@@ -1,6 +1,29 @@
 ﻿namespace DesignPatterns.Composite
 {
-    // TODO: 2.2a Create Selection class that implements Composite pattern and implements IShape interface.
+    // Create Selection class that implements Composite pattern and implements IShape interface.
+
+    class Selection : Shape
+    {
+        public void AddShape(Shape shape)
+        {
+            shapes.Add(shape);
+        }
+
+        public void RemoveShape(Shape shape)
+        {
+            shapes.Remove(shape);
+        }
+
+        public void Move(int dx, int dy)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Move(dx, dy);
+            }
+        }
+
+        private readonly List<Shape> shapes = new List<Shape>();
+    }
 
     static internal class Program
     {
@@ -10,25 +33,25 @@
             Circle c2 = new Circle();
             Rectangle r1 = new Rectangle();
 
-            // TODO: 2.2b Uncomment lines below and test the functionality.
+            // Uncomment lines below and test the functionality.
 
             Console.WriteLine("Move first selection:");
-            //var sel = new Selection();
-            //sel.AddShape(c1);
-            //sel.AddShape(r1);
-            //sel.AddShape(c2);
+            var sel = new Selection();
+            sel.AddShape(c1);
+            sel.AddShape(r1);
+            sel.AddShape(c2);
 
-            //sel.Move(3, 4);
+            sel.Move(3, 4);
 
-            //Console.WriteLine("Move selection with removed c1:");
-            //sel.RemoveShape(c1);
-            //sel.Move(1, 0);
+            Console.WriteLine("Move selection with removed c1:");
+            sel.RemoveShape(c1);
+            sel.Move(1, 0);
 
-            //Console.WriteLine("Move another selection:");
-            //var anotherSel = new Selection();
-            //anotherSel.AddShape(sel);
-            //anotherSel.AddShape(c1);
-            //anotherSel.Move(2, -3);
+            Console.WriteLine("Move another selection:");
+            var anotherSel = new Selection();
+            anotherSel.AddShape(sel);
+            anotherSel.AddShape(c1);
+            anotherSel.Move(2, -3);
         }
     }
 }
