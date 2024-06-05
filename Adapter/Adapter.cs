8ddf06210f649fb8f88bd02fdaf7aa58309d4b53
Marko Pavić <mapavic@vsite.hr>
2024-06-05 19:01:46 +0200

    Implement Adapter

--- Adapter/Adapter.cs
+++ Adapter/Adapter.cs
@@ -2,32 +2,38 @@
 
 namespace DesignPatterns.Adapter
 {
-    class XmlPersonAdapter
-    {
-        public XmlPersonAdapter(string filename)
-        {
-            doc.Load(filename);
-        }
+	class XmlPersonAdapter
+	{
+		public XmlPersonAdapter(string filename)
+		{
+			doc.Load(filename);
+		}
 
-        // TODO: 2.0 Implement GetNames using XmlDocument.GetElementsByTagName method.
-        public IEnumerable<string> GetNames()
-        {
-            List<string> result = new List<string>();
-            return result;
-        }
-        // https://docs.microsoft.com/en-us/dotnet/api/system.xml.xmldocument
-        readonly XmlDocument doc = new XmlDocument();
-    }
+		//  2.0 Implement GetNames using XmlDocument.GetElementsByTagName method.
+		public IEnumerable<string> GetNames()
+		{
+			return doc.GetElementsByTagName("name").Cast<XmlElement>().Select(el=>el.InnerText);
+			//List<string> result = new List<string>();
+			//var NodeList = doc.GetElementsByTagName("name");
+			//foreach (XmlElement node in NodeList)
+			//{
+			//	result.Add(node.InnerText);
+			//}
+			//return result;
+		}
+		// https://docs.microsoft.com/en-us/dotnet/api/system.xml.xmldocument
+		readonly XmlDocument doc = new XmlDocument();
+	}
 
-    internal static class Program
-    {
-        static void Main(string[] args)
-        {
-            XmlPersonAdapter adapter = new XmlPersonAdapter("names.xml");
-            foreach (var name in adapter.GetNames())
-            {
-                Console.WriteLine(name);
-            }
-        }
-    }
+	internal static class Program
+	{
+		static void Main(string[] args)
+		{
+			XmlPersonAdapter adapter = new XmlPersonAdapter("names.xml");
+			foreach (var name in adapter.GetNames())
+			{
+				Console.WriteLine(name);
+			}
+		}
+	}
 }
