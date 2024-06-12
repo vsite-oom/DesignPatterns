64cc09ce28455adf80572413c55c9c782d75fcc4
Hrvoje Janđel <hrjandje@vsite.hr>
2024-06-12 18:58:41 +0200

    Implement command

--- Command/Command.cs
+++ Command/Command.cs
@@ -2,11 +2,35 @@
 {
     interface ICommand
     {
+        
         void Execute();
         void Undo();
     }
 
     // TODO: 3.1a Create MoveShapeCommand class that implements ICommand.
+
+    class MoveShapeCommand : ICommand
+    {
+        public MoveShapeCommand(IShape shape, int dx, int dy)
+        {
+            this.shape = shape;
+            this.dx = dx;
+            this.dy = dy;
+        }
+
+        private readonly IShape shape;
+        private readonly int dx;
+        private readonly int dy;
+        public void Execute()
+        {
+            shape.Move(dx, dy);
+        }
+
+        public void Undo()
+        {
+            shape.Move(-dx, -dy);
+        }
+    }
 
     class Program
     {
@@ -23,12 +47,22 @@
 
             // TODO: 3.1b Uncomment statements below and run the program.
 
-            //MoveShapeCommand cmd = new MoveShapeCommand(sel, 3, 7);
-            //Console.WriteLine("Execute move shape command");
-            //cmd.Execute();
+            MoveShapeCommand cmd = new MoveShapeCommand(sel, 3, 7);
+            Console.WriteLine("Execute move shape command");
+            cmd.Execute();
 
-            //Console.WriteLine("Undo move shape command");
-            //cmd.Undo();
+            Console.WriteLine("Undo move shape command");
+            cmd.Undo();
+
+            Console.WriteLine("Execute resize shape command");
+            var resizeComand= new ResizeShapeCommand(sel, 2, 4);
+            resizeComand.Execute();
+            
+            Console.WriteLine("Undo resize shape command");
+            
+            resizeComand.Undo();
+
+
 
         }
     }
